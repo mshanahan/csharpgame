fda2cfcd7cf7ac39e390c047905c481114631674
Michael Shanahan <mshanahan@huskers.unl.edu>
2017-09-14 09:21:45 -0500

    added Text constructor

--- csharpgame/Text.cs
+++ csharpgame/Text.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,22 @@ namespace csharpgame
     {
 
         public string Contents { get; set; }
-        public int XPos { get; set; }
-        public int YPos { get; set; }
+        public float XPos { get; set; }
+        public float YPos { get; set; }
         public float Transparency { get; set; }
+        public float decayRate { get; set; }
+        public float xMovement { get; set; }
+        public float yMovement { get; set; }
 
+        public Text(string Contents, int XPos, int YPos, float decayRate, float xMovement, float yMovement)
+        {
+            this.Contents = Contents;
+            this.XPos = XPos;
+            this.YPos = YPos;
+            this.Transparency = 1.0F;
+            this.decayRate = decayRate;
+            this.xMovement = xMovement;
+            this.yMovement = yMovement;
+        }
     }
 }
