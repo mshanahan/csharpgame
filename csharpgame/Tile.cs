fda924c5ccd6843fa9322424b0c69600d19b81a9
Michael Shanahan <mshanahan@huskers.unl.edu>
2017-09-13 19:36:42 -0500

    added Tile constructor

--- csharpgame/Tile.cs
+++ csharpgame/Tile.cs
@@ -10,10 +10,20 @@ namespace csharpgame
 {
     class Tile
     {
+        public enum Type { Dirt }
+        private Type type { get; set; }
         private Texture2D texture { get; set; }
-        private Vector2 position { get; set; }
+        private Vector2 gridPos { get; set; }
         private Character onboard { get; set; }
 
-        //public Tile()
+        public Tile(Type type,GraphicsDevice graphicsDevice,Vector2 gridPos)
+        {
+            this.type = type;
+            this.gridPos = gridPos;
+            if(type == Type.Dirt)
+            {
+                this.texture = new Texture2D(graphicsDevice, 50, 50);
+            }
+        }
     }
 }
