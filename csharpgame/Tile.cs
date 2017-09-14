e61478430e7adfd89e404ba9e18fbb94132000cb
Michael Shanahan <mshanahan@huskers.unl.edu>
2017-09-13 19:08:22 -0500

    added data fields to Tile class

--- csharpgame/Tile.cs
+++ csharpgame/Tile.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace csharpgame
 {
     class Tile
     {
+        private Texture2D texture { get; set; }
+        private Vector2 position { get; set; }
+        private Character onboard { get; set; }
     }
 }
