8751a3d39032ccd56c4925b85404f9d4cb1705db
Michael Shanahan <mshanahan@huskers.unl.edu>
2017-09-15 20:29:47 -0500

    added Corpse constructor and data members

--- csharpgame/Corpse.cs
+++ csharpgame/Corpse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace csharpgame
 {
     class Corpse
     {
+        public Texture2D Texture { get; set;  }
+        public Tile Position { get; set; }
+        public float Rotation { get; set; }
+
+        public Corpse(Texture2D Texture, Tile Position, float Rotation)
+        {
+            this.Texture = Texture;
+            this.Position = Position;
+            this.Rotation = Rotation;
+        }
     }
 }
