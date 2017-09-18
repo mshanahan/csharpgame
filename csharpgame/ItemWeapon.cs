6772ea4c51b2e8953c5c8f2556156141d1b27196
Michael Shanahan <mshanahan@huskers.unl.edu>
2017-09-18 09:57:06 -0500

    added ItemWeapon class

---
+++ csharpgame/ItemWeapon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpgame
+{
+
+    class ItemWeapon : Item
+    {
+        public int Dice { get; set; }
+        public int Sides { get; set; }
+        public ItemWeapon(int price, int dice, int sides) : base(price)
+        {
+            this.Dice = dice;
+            this.Sides = sides;
+        }
+
+        public Tuple<int,int> GetDamage()
+        {
+            int min = this.Dice;
+            int max = this.Dice * this.Sides;
+            return new Tuple<int, int>(min, max);
+        }
+
+        public int RollDamage()
+        {
+            int damage = 0;
+            Environment env = Environment.Current();
+            for(int i=0;i<Dice;i++)
+            {
+                damage += env.Random.Next(1, this.Sides + 1);
+            }
+            return damage;
+        }
+    }
+}
