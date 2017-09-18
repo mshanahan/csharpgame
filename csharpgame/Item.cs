c3315602e14df0b383cd705f162e1cef183d62bf
Michael Shanahan <mshanahan@huskers.unl.edu>
2017-09-18 09:49:16 -0500

    implemented Item class

---
+++ csharpgame/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpgame
+{
+    class Item
+    {
+        public int Price { get; set; }
+
+        public Item(int Price)
+        {
+            this.Price = Price;
+        }
+    }
+}
