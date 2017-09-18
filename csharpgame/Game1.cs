df6be422709fb91175d0ec563867c4eb67d3f28c
Michael Shanahan <mshanahan@huskers.unl.edu>
2017-09-18 00:11:56 -0500

    added a new map

--- csharpgame/Game1.cs
+++ csharpgame/Game1.cs
@@ -117,7 +117,7 @@ namespace csharpgame
             WeightList.Add(GoblinWeight);
             Tuple<int, Action<Tile>> KoboldWeight = new Tuple<int, Action<Tile>>(2, new Action<Tile>(CharKobold.Spawn));
             WeightList.Add(KoboldWeight);
-            env.ReadMap("Content/Maps/prototype2.txt", WeightList);
+            env.ReadMap("Content/Maps/prototype3.txt", WeightList);
             
             CharPlayer player = CharPlayer.GetPlayer();
             env.Setup(this, player);
@@ -387,7 +387,7 @@ namespace csharpgame
         {
             env = Environment.Current();
             env.Player.TorchTicks++;
-            if(env.Player.TorchTicks == 120)
+            if(env.Player.TorchTicks == 75)
             {
                 env.Player.TorchTicks = 0;
                 env.Player.TorchCount--;
